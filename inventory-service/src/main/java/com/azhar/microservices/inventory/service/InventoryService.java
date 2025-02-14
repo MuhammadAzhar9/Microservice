3572e18391da53bd627e1c16e35e320fe9484c01
Muhammad Azhar <117720953+MuhammadAzhar9@users.noreply.github.com>
2025-02-14 18:50:45 +0700

    Finish Inventory Service

---
+++ inventory-service/src/main/java/com/azhar/microservices/inventory/service/InventoryService.java
@@ -0,0 +1,20 @@
+package com.azhar.microservices.inventory.service;
+
+import org.springframework.stereotype.Service;
+
+import com.azhar.microservices.inventory.repository.InventoryRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+
+
+public class InventoryService {
+    private final InventoryRepository inventoryRepository;
+
+    public boolean isInStock(String skuCode, Integer quantity) {
+        // Find an inventory for a given skuCode where quantity is greater than 0.
+        return inventoryRepository.existsBySkuCodeAndQuantityGreaterThanEqual(skuCode, quantity); 
+    }
+}
