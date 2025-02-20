2498147f325cfc0496c5fba3bbacba4d93e36279
Muhammad Azhar <117720953+MuhammadAzhar9@users.noreply.github.com>
2025-02-20 15:17:43 +0700

    Finish REST API

---
+++ order-service/src/main/java/com/azhar/microservices/order/config/RestClientConfig.java
@@ -0,0 +1,28 @@
+package com.azhar.microservices.order.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestClient;
+import org.springframework.web.client.support.RestClientAdapter;
+import org.springframework.web.service.invoker.HttpServiceProxyFactory;
+
+import com.azhar.microservices.order.client.InventoryClient;
+
+@Configuration
+public class RestClientConfig {
+
+    @Value("${inventory.url}")
+    private String inventoryServiceUrl;
+
+    @Bean
+    public InventoryClient InventoryClient() {
+        RestClient restClient = RestClient.builder()
+                .baseUrl(inventoryServiceUrl)
+                .build();
+        var restClientAdapter = RestClientAdapter.create(restClient);
+        var httpServiceProxyFactory = HttpServiceProxyFactory.builderFor(restClientAdapter).build();
+        return httpServiceProxyFactory.createClient(InventoryClient.class);
+
+    }
+}
